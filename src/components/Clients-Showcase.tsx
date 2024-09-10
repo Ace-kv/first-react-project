051e8fe6dc1b38015cf5f076f65cdfb225512dbc
Ace-kv <talhashahid100@yahoo.com>
2024-09-10 14:37:49 +0500

    added client showcase component import paths separately to resolve the images not showing in the deployed version. (test1)

--- src/components/Clients-Showcase.tsx
+++ src/components/Clients-Showcase.tsx
@@ -1,10 +1,19 @@
-const clients = [1, 2, 3, 4, 5, 6, 7]
+import client1 from '../assets/images/client-1.png'
+import client2 from '../assets/images/client-2.png'
+import client3 from '../assets/images/client-3.png'
+import client4 from '../assets/images/client-4.png'
+import client5 from '../assets/images/client-5.png'
+import client6 from '../assets/images/client-6.png'
+import client7 from '../assets/images/client-7.png'
+
+const clients = [client1, client2, client3, client4, client5, client6, client7]
 
 const ClientShowcase = () => {
+
     return (
         <>
-            {clients.map((client: number) => (
-                <img src={`/src/assets/images/client-${client}.png`} alt={`client-logo-${client}`} />
+            {clients.map((client, index) => (
+                <img src={client} alt={`client-logo-${index + 1}`} />
             ))}
         </>
     )
