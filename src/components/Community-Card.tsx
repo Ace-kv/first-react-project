74ecd156b203667360789cc68a5ea05b25ba379f
Ace-kv <talhashahid100@yahoo.com>
2024-09-11 12:39:58 +0500

    Added an iconMap mapping the separately imported images and then used in the Community-Card.tsx, to see if this works in deployment for images

--- src/components/Community-Card.tsx
+++ src/components/Community-Card.tsx
@@ -1,10 +1,20 @@
+import memberIcon from '../../public/assets/icons/member-org-icon.png'
+import natAsocIcon from '../../public/assets/icons/national-assoc-icon.png'
+import clubsNgroupsIcon from '../../public/assets/icons/clubsNgroups-icon.png'
+
+const iconMap: { [key: string]: string } = {             // index signature to allow dynamic keys
+    'member-org-icon.png': memberIcon,
+    'national-assoc-icon.png': natAsocIcon,
+    'clubsNgroups-icon.png': clubsNgroupsIcon
+}
+
 const CommunityCard = ({imageFileName, hTextT, hTextB, pText  }: {
     imageFileName: string,
     hTextT: string,
     hTextB: string,
     pText: string
 }) => {
-    const imageFile = `../../public/assets/icons/${imageFileName}`
+    const imageFile: string = iconMap[imageFileName] || ''
     
     return (
         <div className="community-card">
